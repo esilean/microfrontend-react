e52f854a26e73d3a4784cb3db193da369161641c
Leandro Bevilaqua <le.bevilaqua@gmail.com>
2021-03-17 20:55:09 -0300

    added auth action

--- auth/src/App.js
+++ auth/src/App.js
@@ -6,7 +6,7 @@ import Signin from './components/Signin'
 import Signup from './components/Signup'
 
 const generateClassName = createGenerateClassName({
-    productionPrefix: 'auth'
+    productionPrefix: 'aut'
 })
 
 export default ({ history }) => {
